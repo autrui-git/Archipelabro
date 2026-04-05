2e8db8cae78d3ee2acea2664328e9266c7dfd55c
autrui-git <gwenn.cf@gmail.com>
2026-04-05 20:39:51 +0200

    Ajoutez des fichiers projet.

---
+++ Main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.Enums;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityModManagerNet;
+using Utility.Platforms;
+using Utility.Platforms.Desktop;
+
+namespace Archipelabro
+{
+    static class Main
+    {
+        public static ArchipelagoSession session = ArchipelagoSessionFactory.CreateSession("localhost", 38281);
+        private static void Connect(String server, string user, string pass)
+        {
+
+            LoginResult result;
+
+            try
+            {
+                result = session.TryConnectAndLogin("Broforce", "Gwenn", Archipelago.MultiClient.Net.Enums.ItemsHandlingFlags.AllItems);
+            }
+            catch (Exception e)
+            {
+                result = new LoginFailure(e.GetBaseException().Message);
+            }
+            if (!result.Successful)
+            {
+                LoginFailure failure = (LoginFailure)result;
+                string errorMessage = $"Failed to Connect to {server} as {user}:";
+                foreach (string error in failure.Errors)
+                {
+                    errorMessage += $"\n    {error}";
+                }
+                foreach (ConnectionRefusedError error in failure.ErrorCodes)
+                {
+                    errorMessage += $"\n    {error}";
+                }
+
+                return; // Did not connect, show the user the contents of `errorMessage`
+            }
+
+            // Successfully connected, `ArchipelagoSession` (assume statically defined as `session` from now on) can now be
+            // used to interact with the server and the returned `LoginSuccessful` contains some useful information about the
+            // initial connection (e.g. a copy of the slot data as `loginSuccess.SlotData`)
+            var loginSuccess = (LoginSuccessful)result;
+        }
+    }
+}
